805e8e351729771902fdaf8a52c99b0c7b0f381b
yakovbenalex <yakovbenalex@mail.ru>
2022-02-21 00:41:24 +0300

    Initial commit

---
+++ app/src/main/java/ru/opalevapps/andersenandroidlesson3flags/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.opalevapps.andersenandroidlesson3flags
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
